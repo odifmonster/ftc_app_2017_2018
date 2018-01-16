4b6a451f6d9593c25351e367d65c3d36ab1b7283
odifmonster <lamanwyner@icloud.com>
2018-01-16 12:08:50 -0500

    DogeCV dependencies and other things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGlyphDetectMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.detectors.GlyphDetector;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by lamanwyner on 1/16/18.
+ */
+
+@TeleOp
+public class TestGlyphDetectMode extends LinearOpMode {
+    private GlyphDetector glyphDetector;
+
+    public void runOpMode() {
+        glyphDetector = new GlyphDetector();
+        glyphDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        glyphDetector.enable();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Closest Glyph Pos", glyphDetector.getChosenGlyphPosition().toString());
+            telemetry.addData("Closest Glyph Offset", glyphDetector.getChosenGlyphOffset());
+            telemetry.update();
+        }
+
+        glyphDetector.disable();
+    }
+}
