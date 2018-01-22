2f9e531b16faad27e8b8217f510cc49bfa893438
odifmonster <lamanwyner@icloud.com>
2018-01-22 17:47:09 -0500

    testing mode for driving library

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrivingLibraryAuton.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.libraries.DrivingLibrary;
+import org.firstinspires.ftc.whatever.DrivingLib;
+
+/**
+ * Created by lamanwyner on 1/22/18.
+ */
+
+@Autonomous
+public class TestDrivingLibraryAuton extends LinearOpMode {
+    DrivingLibrary drivingLibrary;
+
+    @Override
+    public void runOpMode() {
+        drivingLibrary = new DrivingLibrary(this);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drivingLibrary.turnRight(Math.PI / 2);
+        }
+    }
+}
