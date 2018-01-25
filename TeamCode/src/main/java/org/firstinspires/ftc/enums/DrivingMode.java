a4f55939c349adf663f428f808d7a3499377b13f
odifmonster <lamanwyner@icloud.com>
2018-01-25 16:43:14 -0500

    fgshdfj

--- TeamCode/src/main/java/org/firstinspires/ftc/enums/DrivingMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/enums/DrivingMode.java
@@ -5,5 +5,16 @@ package org.firstinspires.ftc.enums;
  */
 
 public enum DrivingMode {
-    FLOAT_STOP, BRAKE_STOP
+    FLOAT_STOP("Float"),
+    BRAKE_STOP("Brake");
+
+    String stringValue;
+
+    DrivingMode(String s) {
+        stringValue = s;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
 }
