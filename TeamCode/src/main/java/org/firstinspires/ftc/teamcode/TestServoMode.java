3979833cee7046fd373ef559b7b2d9e14b48ac4d
Megan Kaye <megan.kaye38@gmail.com>
2018-02-22 09:17:15 -0500

    lakshklasjhlkashasklhkasj

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServoMode.java
@@ -34,38 +34,45 @@ public class TestServoMode extends LinearOpMode {
 
             if (gamepad2.left_bumper) {
                 //left top
+                telemetry.addData("lefttpushed", true);
                 if (leftt) {
                     leftTop.setPosition(1);
                 } else {
                     leftTop.setPosition(0);
                 }
+                leftt = !leftt;
             }
             if (gamepad2.right_bumper) {
                 //right top
+                telemetry.addData("rightttpushed", true);
                 if (rightt) {
                     rightTop.setPosition(1);
                 } else {
                     rightTop.setPosition(0);
                 }
+                rightt = !rightt;
 
             }
             if (gamepad2.left_trigger > threshold) {
                 //left bottom
+                telemetry.addData("leftbpushed", true);
                 if (leftb) {
                     leftBottom.setPosition(1);
                 } else {
                     leftBottom.setPosition(0);
                 }
+                leftb = !leftb;
 
             }
             if (gamepad2.right_trigger > threshold) {
                 //right bottom
+                telemetry.addData("rightbpushedd", true);
                 if (rightb) {
                     rightBottom.setPosition(1);
                 } else {
                     rightBottom.setPosition(0);
                 }
-
+                rightb = !rightb;
             }
 
             telemetry.addData("leftbottom", leftBottom.getPosition());
