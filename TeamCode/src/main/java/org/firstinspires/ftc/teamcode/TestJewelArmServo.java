faf667e781499db2d7e4528aa0b4811277df6b7a
Megan Kaye <megan.kaye38@gmail.com>
2018-02-25 14:29:26 -0500

    kljhlkas

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestJewelArmServo.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.enums.FTCAlliance;
+import org.firstinspires.ftc.enums.FTCPosition;
+import org.firstinspires.ftc.libraries.AutonModeLibrary;
+
+/**
+ * Created by megankaye on 2/25/18.
+ */
+@TeleOp
+public class TestJewelArmServo extends LinearOpMode {
+    AutonModeLibrary autonMode;
+
+    public void runOpMode() throws InterruptedException {
+        autonMode = new AutonModeLibrary(this, FTCAlliance.RED, FTCPosition.LEFT, true);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            autonMode.testMoveServo();
+
+            sleep(30000);
+        }
+    }
+}
